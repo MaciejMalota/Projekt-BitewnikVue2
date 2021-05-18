517d37fa1600e1fa9fc71d1940fffa10420d1ca3
Maciej Małota <maciejmny@gmail.com>
2021-05-18 12:47:09 +0200

    x

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,48 +1,53 @@
 <template>
-  <div class="wrapper" style="background-image: url('images/bg-registration-form-1.jpg');">
-			<div class="inner">
-				<div class="image-holder">
-					<img src="images/registration-form-1.jpg" alt="">
-				</div>
-				<form action="">
-					<h3>Registration Form</h3>
-					<div class="form-group">
-						<input type="text" placeholder="First Name" class="form-control">
-						<input type="text" placeholder="Last Name" class="form-control">
-					</div>
-					<div class="form-wrapper">
-						<input type="text" placeholder="Username" class="form-control">
-						<i class="zmdi zmdi-account"></i>
-					</div>
-					<div class="form-wrapper">
-						<input type="text" placeholder="Email Address" class="form-control">
-						<i class="zmdi zmdi-email"></i>
-					</div>
-					<div class="form-wrapper">
-						<select name="" id="" class="form-control">
-							<option value="" disabled selected>Gender</option>
-							<option value="male">Male</option>
-							<option value="femal">Female</option>
-					
-						</select>
-						<i class="zmdi zmdi-caret-down" style="font-size: 17px"></i>
-					</div>
-					<div class="form-wrapper">
-						<input type="password" placeholder="Password" class="form-control">
-						<i class="zmdi zmdi-lock"></i>
-					</div>
-					<div class="form-wrapper">
-						<input type="password" placeholder="Confirm Password" class="form-control">
-						<i class="zmdi zmdi-lock"></i>
-					</div>
-					<button>Register
-						<i class="zmdi zmdi-arrow-right"></i>
-					</button>
-				</form>
-			</div>
-		</div>
-</template>
+  <!-- Material form register -->
+  <div class="back">
+  <br>
+    <form class ="siema">
+     
+      <div class="white-text">
+       <p class="h4 text-center mb-4">Rejestracja Użytkownika</p>
+        <mdb-input label="Imię" icon="user" type="text"/>
+        <mdb-input label="Nazwisko" icon="user" type="text"/>
+        <mdb-input label="Data urodzenia" icon="calendar-day" type="text"/>
 
-<style scoped>
+        <mdb-input label="Twój e-mail" icon="envelope" type="email"/>
+        <mdb-input label="Potwierdź e-mail" icon="exclamation-triangle" type="text"/>
+        <mdb-input label="Twoje hasło" icon="lock" type="password"/>
+        
+      </div>
+      <div class="text-center">
+        <mdb-btn color="success">Zarejestruj</mdb-btn>
+      </div>
+    </form>
+    <!-- Material form register -->
+  </div>
+</template>
+<script>
+  import { mdbInput, mdbBtn } from 'mdbvue';
+  export default {
+    name: 'Basic',
+    components: {
+      mdbInput,
+      mdbBtn
+    }
+  }
+</script>
+<style>
+body,html{
+   background-image: url('https://images.contentstack.io/v3/assets/bltad9188aa9a70543a/blte06167f518dd1579/5f5c00ebfef72a4b5f96597f/01-Header.jpg?width=3200&height=1800');
+  background-repeat: no-repeat;
+  background-size: 100vw 100vh;
+}
+.white-text{
+  max-width:50vh;
+  margin:auto;
+}
+input::-webkit-input-placeholder {
+        color: red;
+}
+
+input:-moz-placeholder {
+        color: red;
+}
 
 </style>
