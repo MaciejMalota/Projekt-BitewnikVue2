2babf20437854028e57457fdedbe0e3a1f36fb66
Maciej Małota <maciejmny@gmail.com>
2021-05-18 14:02:48 +0200

    xxx

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,3 +1,13 @@
 <template>
   <div>Teams</div>
 </template>
+<script>
+
+export default {
+  components: {
+  },
+  beforeCreate: function() {          // do body background w global.css
+        document.body.className = 'teams';
+    }
+}
+</script>
