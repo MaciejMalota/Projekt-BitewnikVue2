ce1c79d77b7da1f610673651f107b62a05aa3dc4
Maciej Małota <maciejmny@gmail.com>
2021-05-24 01:16:03 +0200

    Tworzenie turnieji

--- src/views/AddTournament.vue
+++ src/views/AddTournament.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>TWORZE TURNIEJ</div>
+      <div class="formTurnieju">
+    <p class="h4 text-center mb-4">Sign in</p>
+    <div class="black-text">
+        <mdb-input label="Gra" icon="gamepad" type="text"/>
+        <mdb-input label="Nazwa turnieju" icon="book-open" type="text"/>
+        <mdb-input label="Nagroda" icon="trophy" type="text"/>
+        <mdb-input label="Data eventu" v-model ="tournament.data" icon="calendar-day" type="text"/>
+        <mdb-input label="Ulica" icon="road" type="text"/>
+        <mdb-input label="Miasto" icon="city" type="text"/>
+    </div>
+    <div class="text-center">
+      <mdb-btn color="success">Stwórz</mdb-btn>
+    </div>
+  </div>
 </template>
 <script>
+import { mdbInput, mdbBtn } from 'mdbvue';
 
 export default {
      data(){
@@ -12,17 +26,23 @@ export default {
           Game: "League of Legends",
           Title: "Turniej Ligi",
           Prize: "100 zł",
-          data: "1998/11/30",
-          Adres: "ul.Kossaka 12",
-          miasto: "Nysa",
+          Data: "2021/11/30",
+          Ulica: "ul.Kossaka 12",
+          Miasto: "Nysa",
         //   regulamin:
         }
       }
     },
   components: {
+      mdbInput,
+      mdbBtn
   },
   beforeCreate: function() {          // do body background w global.css
         document.body.className = 'home';
     }
 }
 </script>
+
+<style scoped>
+
+</style>
