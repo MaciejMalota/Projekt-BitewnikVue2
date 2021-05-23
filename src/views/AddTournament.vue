74fef2d5ea37fca4b1d121ecf6c47413b26aec7c
Maciej Małota <maciejmny@gmail.com>
2021-05-24 01:18:42 +0200

    x

--- src/views/AddTournament.vue
+++ src/views/AddTournament.vue
@@ -2,12 +2,12 @@
       <div class="formTurnieju">
     <p class="h4 text-center mb-4">Sign in</p>
     <div class="black-text">
-        <mdb-input label="Gra" icon="gamepad" type="text"/>
-        <mdb-input label="Nazwa turnieju" icon="book-open" type="text"/>
-        <mdb-input label="Nagroda" icon="trophy" type="text"/>
-        <mdb-input label="Data eventu" v-model ="tournament.data" icon="calendar-day" type="text"/>
-        <mdb-input label="Ulica" icon="road" type="text"/>
-        <mdb-input label="Miasto" icon="city" type="text"/>
+        <mdb-input label="Gra" v-model ="tournament.Game" icon="gamepad" type="text"/>
+        <mdb-input label="Nazwa turnieju" v-model ="tournament.Title" icon="book-open" type="text"/>
+        <mdb-input label="Nagroda" v-model ="tournament.Prize" icon="trophy" type="text"/>
+        <mdb-input label="Data eventu" v-model ="tournament.Data" icon="calendar-day" type="text"/>
+        <mdb-input label="Ulica" v-model ="tournament.Street" icon="road" type="text"/>
+        <mdb-input label="Miasto" v-model ="tournament.City" icon="city" type="text"/>
     </div>
     <div class="text-center">
       <mdb-btn color="success">Stwórz</mdb-btn>
@@ -27,8 +27,8 @@ export default {
           Title: "Turniej Ligi",
           Prize: "100 zł",
           Data: "2021/11/30",
-          Ulica: "ul.Kossaka 12",
-          Miasto: "Nysa",
+          Street: "ul.Kossaka 12",
+          City: "Nysa",
         //   regulamin:
         }
       }
