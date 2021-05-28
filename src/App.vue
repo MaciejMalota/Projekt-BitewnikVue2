886c7e509a0783670a7291d9b2a2a623468afabb
Patryk Laskowski <lasekl1998@gmail.com>
2021-05-28 22:26:37 +0200

    weryfikacja

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,14 @@
 
 <script>
 import AppHeader from './components/AppHeader.vue'
+
 export default {
   name: 'App',
   components: {
     AppHeader
+  },
+  beforeCreate: function(){
+
   },
   data() {
     return {
