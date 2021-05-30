13aba11ae366b25101eedb13fcbe2aecbdbb5d92
Maciej Małota <maciejmny@gmail.com>
2021-05-30 15:34:47 +0200

    x

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,59 +1,46 @@
 <template>
-
-<b-container >
-    <b-row align-v="stretch"  no-gutters >
-    <b-col   md="8" class="t1" >
-
- <b-carousel
-    id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000"
-    fade
-    controls
-    indicators
-    img-width="1040"
-    img-height="480"
-   
-  >
-    <b-carousel-slide
-      caption="Counter-Strike"
-      img-src="https://www.telepolis.pl/images/2021/03/counter-strike-global-offensive.jpg"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="League of Legends"
-      img-src="https://how2play.pl/wp-content/uploads/2016/04/baron-nashor-from-league-of-legends.jpg"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="DOTA 2"
-      img-src="https://wallpaperaccess.com/full/671214.jpg"
-    ></b-carousel-slide>
-  </b-carousel>
-
-  </b-col>
-    <b-col class="t2">
-      
-  <b-img src="https://static01.nyt.com/images/2014/10/12/business/12-LEGENDS-1/12-LEGENDS-1-superJumbo.jpg" fluid alt="Responsive image"></b-img>
-  <b-img style="height:15vh;width:40vh" src="http://mikowhy.pl/wp-content/uploads/2018/10/CS-GO-tournament.jpg" fluid alt="Responsive image"></b-img>
-
-    </b-col>
-    
-  </b-row>
-
-  <!-- KOLEJNY -->
-
-    <!-- <b-row>
-    <b-col>1 of 3</b-col>
-    <b-col>2 of 3</b-col>
-    <b-col>3 of 3</b-col>
-  </b-row> -->
-  <!-- JESZCZE 1 -->
-    <!-- <b-row>
-    <b-col>1 of 3</b-col>
-    <b-col>2 of 3</b-col>
-    <b-col>3 of 3</b-col>
-  </b-row> -->
-
-</b-container>
-
+  <div class="cont">
+    <div class="t1">
+      <b-carousel
+        id="carousel-fade"
+        style="text-shadow: 0px 0px 2px #000"
+        fade
+        controls
+        indicators
+        img-width="1040"
+        img-height="480"
+        class="karuzela"
+      >
+        <b-carousel-slide
+          caption="First slide"
+          img-src="https://picsum.photos/1024/480/?image=10"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Second Slide"
+          img-src="https://picsum.photos/1024/480/?image=12"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Third Slide"
+          img-src="https://picsum.photos/1024/480/?image=22"
+        ></b-carousel-slide>
+      </b-carousel>
+    </div>
+    <div class="drugi">
+      <H2>drugi element</H2>
+    </div>
+    <div class="trzeci">
+      <H2>trzeci element</H2>
+    </div>
+    <div class="czwarty">
+      <H2>czwarty element</H2>
+    </div>
+    <div class="piaty">
+      <H2>piaty element</H2>
+    </div>
+    <div class="szosty">
+      <H2>szosty element</H2>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -67,21 +54,18 @@ export default {
 </script>
 
 <style scoped>
-.container{
-margin-top: 5vw;
-width: 1200vw;
-height: 40vh;
-border: solid 20px;
+.cont {
+  width: 80%;
+  margin: auto;
+  height: 100%;
+  margin-top: 5rem;
+  border: solid 20px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: minmax(230px, auto);
 }
-.t1{
- 
-  height: 37vh;
-  /* height: auto; */
-  /* width: 40vw; */
-}
-.t2{
- 
-  height: 40vh;
+.t1 {
+  border: solid 4px;
   /* height: auto; */
   /* width: 40vw; */
   grid-column: 1 / 3;
@@ -112,23 +96,4 @@ border: solid 20px;
   grid-column: 3;
   grid-row: 3;
 }
-
-.carousel{
-  height: 37vh;
-  max-width: 100vw;
-
-}
-.carousel .carousel-item {
-  height: 37vh;
-  
-    object-fit:cover;
-    top: 0;
-    left: 0;
-}
-
-
-
-
-
-
 </style>
--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,36 +1,12 @@
 <template>
-<div class="container">
-    <h1>Responsive Bootstrap</h1>
-    <div class="row">
-        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5" style="background-color:red;">Span 5</div>
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" style="background-color:blue">Span 3</div>
-        <div class="col-lg-2 col-md-2 col-sm-3 col-xs-2" style="padding:0px">
-            <div class="short-div" style="background-color:green">Span 2</div>
-            <div class="short-div" style="background-color:purple">Span 2</div>
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-3 col-xs-2" style="background-color:yellow">Span 2</div>
-    </div>
-</div>
-
+  <div>aaaa</div>
 </template>
 <script>
-import { mdbInput, mdbBtn } from 'mdbvue';
 export default {
-  components: {
-      // mdbInput,
-      // mdbBtn
-    },
-  beforeCreate: function() {          // do body background w global.css
-        document.body.className = 'teams';
-    }
-}
+  components: {},
+  beforeCreate: function () {
+    // do body background w global.css
+    document.body.className = "teams";
+  },
+};
 </script>
-
-<style scoped>
-div {
-  height:50px;
-}
-.short-div {
-  height:25px;
-}
-</style>
